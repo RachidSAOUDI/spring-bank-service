79009f4c24069b4e8c90c6560a7a46573ad69b5f
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-11 14:52:56 +0100

    Methods Save+update+delete

--- src/main/java/ma/usmba/springbankservice/web/AccountRestController.java
+++ src/main/java/ma/usmba/springbankservice/web/AccountRestController.java
@@ -2,11 +2,11 @@ package ma.usmba.springbankservice.web;
 
 import ma.usmba.springbankservice.entities.BankAccount;
 import ma.usmba.springbankservice.repositories.BankAccountRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class AccountRestController {
@@ -27,6 +27,25 @@ public class AccountRestController {
                 .orElseThrow(()->new RuntimeException(String.format("Account %s Not Found",id)));
     }
 
+    @PostMapping("/bankAccounts")
+    public BankAccount save(@RequestBody BankAccount bankAccount){
+        if (bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
+        return bankAccountRepository.save(bankAccount);
+    }
 
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id, @RequestBody BankAccount bankAccount){
+        BankAccount account = bankAccountRepository.findById(id).orElseThrow();
+        if (bankAccount.getBalance() != null) account.setBalance(bankAccount.getBalance());
+        if (bankAccount.getCreatedAt() != null) account.setCreatedAt(new Date());
+        if (bankAccount.getType() != null) account.setType(bankAccount.getType());
+        if (bankAccount.getCurrency() != null) account.setCurrency(bankAccount.getCurrency());
+        return bankAccountRepository.save(bankAccount);
+    }
+
+    @DeleteMapping("/bankAccounts/{id}")
+    public void deleteAccount(@PathVariable String id){
+        bankAccountRepository.deleteById(id);
+    }
 
 }
